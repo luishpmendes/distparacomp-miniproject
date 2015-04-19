e95ccdff52306b5a5d6ad68ff80d0a48689c89d6
Luis H. P. Mendes <luishpmendes@gmail.com>
2015-04-19 22:11:14 +0100

    miniproject

--- miniproject.cu
+++ miniproject.cu
@@ -248,6 +248,9 @@ __device__ void device_initialSolution (curandState * state, float * x) {
 __device__ void device_crossover (curandState * state, float * h, float * x0, float * x1) {
     for (int i = 0; i < N; i++) {
         h[i] = device_randomUniform(state, x0[i], x1[i]);
+        #if __CUDA_ARCH__>=200
+            printf("%d : crossover = %f\n", id, x[i]);
+        #endif
     }
 }
 
