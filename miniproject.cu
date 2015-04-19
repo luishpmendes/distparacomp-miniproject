ca5c842093b7df89dfb9d0ce38e55be65c8a28f0
Luis H. P. Mendes <luishpmendes@gmail.com>
2015-04-19 16:34:30 +0100

    miniproject

--- miniproject.cu
+++ miniproject.cu
@@ -293,17 +293,21 @@ __global__ void device_findOptimum (float * solution, unsigned int seed) {
                 }
             }
 
-            //#if __CUDA_ARCH__>=200
-            //    printf("thread %d is writing on memory\n", id);
-            //#endif
+            #if __CUDA_ARCH__>=200
+                printf("thread %d is writing on memory\n", id);
+            #endif
 
             __syncthreads();
 
             // get the individual from previous thread and replace worst individual if it is worse than the received one
             
-            //#if __CUDA_ARCH__>=200
-            //    printf("thread %d is reading on memory\n", id);
-            //#endif
+            #if __CUDA_ARCH__>=200
+                printf("thread %d is reading on memory\n", id);
+            #endif
+
+            #if __CUDA_ARCH__>=200
+                printf("%d %f\n", id, device_objectiveFunction(sharedMem[id]));
+            #endif
 
             if (device_objectiveFunction(x0) > device_objectiveFunction(x1)) { // x0 is the worst
                 if (device_objectiveFunction(x0) > device_objectiveFunction(sharedMem[id])) { // x0 is worse than the received one
